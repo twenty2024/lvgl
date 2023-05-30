852782d53d12aaeb0342e2109ebd67b242fed755
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2023-05-30 09:45:14 +0200

    fix horizontal gradient

--- src/draw/sw/lv_draw_sw_rect.c
+++ src/draw/sw/lv_draw_sw_rect.c
@@ -164,6 +164,7 @@ static void draw_bg(lv_draw_unit_t * draw_unit, const lv_draw_rect_dsc_t * dsc, 
     lv_grad_t * grad = lv_gradient_get(&dsc->bg_grad, coords_bg_w, coords_bg_h);
     if(grad && grad_dir == LV_GRAD_DIR_HOR) {
         blend_dsc.src_buf = grad->color_map + clipped_coords.x1 - bg_coords.x1;
+        blend_dsc.src_color_format = LV_COLOR_FORMAT_RGB888;
     }
 
 #if _DITHER_GRADIENT
